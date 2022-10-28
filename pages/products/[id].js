8a09db1a87d5f227c9f2c4c9022d688cc6088b28
Yusuf Emre Velioglu <velioglu.ye@gmail.com>
2022-10-28 13:51:14 +0200

    create fetch function

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,8 +1,8 @@
 import styles from "../../styles/Home.module.css"
 import Image from "next/image";
-import { useRouter } from "next/router"; 
+import { useRouter } from "next/router";
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
     const router = useRouter();
     const { id } = router.query;
 
@@ -19,9 +19,13 @@ const Product = ({product}) => {
 
 export default Product
 
+const getAllProducts = async () => {
+    return await fetch('https://fakestoreapi.com/products')
+        .then(res => res.json());
+}
+
 export async function getStaticPaths() {
-    const products = await fetch('https://fakestoreapi.com/products')
-                        .then(res=>res.json());
+    const products = await getAllProducts();
 
     const paths = products.map((product) => ({
         params: { id: `${product.id}` },
@@ -29,11 +33,10 @@ export async function getStaticPaths() {
     return { paths, fallback: false }
 }
 export async function getStaticProps({ params }) {
-    const products = await fetch('https://fakestoreapi.com/products')
-    .then(res=>res.json());
+    const products = await getAllProducts();
 
     const getProductById = (id) => products.find(obj => obj.id == id)
-    // the params comes from getStaticPath
+
     const product = await getProductById(params.id)
     return { props: { product } }
 }
